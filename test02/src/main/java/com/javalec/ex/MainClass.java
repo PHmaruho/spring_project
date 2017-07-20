b9cfe0984e6fe989c667b5a289722440c9d8856e
phmaruho <phmaruho@192.168.1.30>
2017-07-20 17:32:11 +0900

    commit_test

---
+++ test02/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,16 @@
+package com.javalec.ex;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculation cal = new Calculation();
+		cal.setFirstNum(10);
+		cal.setSecondNum(5);
+		cal.add();
+		cal.sub();
+		cal.mult();
+		cal.div();
+	}
+
+}
